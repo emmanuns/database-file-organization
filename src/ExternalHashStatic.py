96aac0bc58b0dbd7132de1ce6fc2c88ae3dbf97b
Alanna <alanna.simoes@gmail.com>
2024-09-26 21:17:14 -0300

    Teste hash OK

--- src/ExternalHashStatic.py
+++ src/ExternalHashStatic.py
@@ -15,17 +15,28 @@ class Bucket:
             self.overflow.append((key, value))
 
 class ExternalHashStatic:
-    def __init__(self, num_buckets, bucket_size):
+    def __init__(self, num_buckets:int, bucket_size:int):
         self.num_buckets = num_buckets
         self.bucket_size = bucket_size
         self.buckets = [Bucket(bucket_size) for _ in range(num_buckets)]
 
     def hash_function(self, key):
+        if type(key) == str:
+            key = len(key)
         return key % self.num_buckets
 
     def insert(self, key, value):
         index = self.hash_function(key)
         self.buckets[index].insert(key, value)
+
+    def remove(self, key):
+        index = self.hash_function(key)
+        for i in self.buckets:
+            for j, (k, v) in enumerate(i.elements):
+                if k == key:
+                    del self.buckets[j]
+                    return True
+        return False
 
     def search(self, key):
         index = self.hash_function(key)
--- tests/teste_hash.py
+++ tests/teste_hash.py
@@ -7,7 +7,7 @@ from src.ExternalHashStatic import ExternalHashStatic  # Ajuste o caminho confor
 
 class TestExternalHash(unittest.TestCase):
     def setUp(self):
-        self.hash_table = ExternalHashStatic('test_hash_table.pkl')
+        self.hash_table = ExternalHashStatic(3,10)
 
     def test_insert_and_search(self):
         self.hash_table.insert('key1', 'value1')
